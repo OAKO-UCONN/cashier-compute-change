df06f51c43928b93b071e5e8e33479a4e7aea423
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-30 18:38:19 -0400

    Update cashier_compute_change.py

--- cashier_compute_change.py
+++ cashier_compute_change.py
@@ -2,3 +2,15 @@ def give_change(total, cost,d):
     
     c = total-cost
     sortedkeys=storted(d.keys(),reverse=True)
+    print(sortedkeys)
+    for i in sortedkeys:
+        n = c//i
+        c %= i
+        d[i]=n
+        
+        print(d)
+    return d
+
+
+d = {10:0,20:0,5:0,1:0}
+print(give_change(100, 55,d))
